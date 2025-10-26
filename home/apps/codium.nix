5e5d1066156aeac81b0754b8a7263c2d46b4c279
Suezhoo <Suezhoo@outlook.com>
2025-10-26 16:13:03 +0100

    Added formatter and appearance extensions

--- home/apps/codium.nix
+++ home/apps/codium.nix
@@ -6,25 +6,37 @@
         # Use VSCodium instead of VS Code
         package = pkgs.vscodium;
 
-         # Keep extensions managed by Home-Manager (immutable on disk)
+        # Keep extensions managed by Home-Manager (immutable on disk)
         mutableExtensionsDir = false;
 
         # Extensions from nixpkgs' Open VSX set:
         extensions = with pkgs.vscode-extensions; [
-            dracula-theme.theme-dracula
+            # Nix support
             bbenoist.nix
             jnoortheen.nix-ide
-        ];
+
+            # Appearance
+            pkief.material-icon-theme
+            enkia.tokyo-night
+
+            # Formatter
+            esbenp.prettier-vscode
+
+            # ESLint
+            dbaeumer.vscode-eslint
+        ]; 
 
         userSettings = {
-            "workbench.colorTheme" = "Dracula";
+            "workbench.colorTheme" = "Tokyo Night";
+            "workbench.iconTheme" = "material-icon-theme";
 
             # Nix IDE
             "nix.enableLanguageServer" = true;
             "nix.serverPath" = "nil";
             "nix.formatterPath" = "nixfmt";
 
-            # Other
+            # Formatter
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
             "editor.formatOnSave" = true;
         };
     };
