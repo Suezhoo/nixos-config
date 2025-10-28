838df13e18e28996d6f1c22729c7bc85514ac4ef
Suezhoo <suezhoo@outlook.com>
2025-10-28 03:47:29 +0100

    Configuring hyprland and waybar

---
+++ home/hypr/waybar.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ...}
+
+{
+	programs.waybar = {
+	enable = true;
+	settings = {
+		mainBar = {
+			layer ="top";
+			position ="top";
+			height = 32;
+			modules-left=["hyprland/workspaces" "clock"];
+			modules-right=["cpu" "memory" "network" "battery" "tray"];
+		};
+	];
+
+	style = ''
+		*{
+			font-family: JetBrainsMono, sans-serif;
+			font-size: 12px;
+		}
+		window#waybar {
+			background: rgba(30,30,30,0.8);
+			border-radius: 8px;
+		}
+		'';
+	};
+}
--- hosts/sayonara/configuration.nix
+++ hosts/sayonara/configuration.nix
@@ -6,6 +6,7 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/common.nix
+    ../../modules/hyprland.nix
   ];
 
   networking.hostName = "sayonara";
