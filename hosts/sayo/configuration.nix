a062852e11f2e3a51c76ab903c8bd261410aceb4
suezhoo <suezhoo@outlook.com>
2025-11-11 21:03:48 +0100

    Fix: home-manager issue

--- hosts/sayo/configuration.nix
+++ hosts/sayo/configuration.nix
@@ -10,9 +10,28 @@
 
   networking.hostName = "sayo";
 
+  # define the user
+  users.groups.Suezhoo = {};
   users.users.suezhoo = {
     isNormalUser = true;
-    extraGroups = ["wheel"]; #sudo
+    group = "Suezhoo";
+    extraGroups = ["wheel" "networkmanager" "video" "audio" "input"];
+  };
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.suezhoo = import ../../home/suezhoo;
+    backupFileExtension = "hm-backup";
+
+    sharedModules = [
+      inputs.plasma-manager.homeModules.plasma-manager
+    ];
+
+    # Pass host to HM modules
+    extraSpecialArgs = {
+      host = config.networking.hostName;
+    };
   };
 
   # Allow unfree (apps like steam etc)
@@ -27,6 +46,9 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  # Niri
+  services.displayManager.sessionPackages = [pkgs.niri];
 
   # Pin to installed NixOS release; dont bump casually.
   system.stateVersion = "25.05";
