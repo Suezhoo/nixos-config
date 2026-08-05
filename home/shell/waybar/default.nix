c9a507960ca6ce52e711f3909f1d6abbdb665aa8
suezhoo <suezhoo@outlook.com>
2026-08-05 07:36:13 +0200

    feat(waybar): compare multilingual workspace labels

--- home/shell/waybar/default.nix
+++ home/shell/waybar/default.nix
@@ -5,7 +5,10 @@
   host,
   ...
 }: {
-  home.packages = [pkgs.nerd-fonts.jetbrains-mono];
+  home.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    noto-fonts-cjk-sans
+  ];
 
   programs.waybar = {
     enable = true;
@@ -14,12 +17,22 @@
       mainBar = {
         layer = "bottom";
         "margin-top" = 10;
-        "margin-left" = 10;
-        "margin-right" = 10;
+        "margin-left" = 20;
+        "margin-right" = 20;
         position = "top";
         height = 32;
 
-        modules-left = ["custom/os" "hyprland/workspaces" "niri/workspaces" "niri/window"];
+        modules-left = [
+          "custom/os"
+          "niri/workspaces#japanese"
+          "niri/workspaces#korean"
+          "niri/workspaces#chinese"
+          "niri/workspaces#english"
+          "hyprland/workspaces#japanese"
+          "hyprland/workspaces#korean"
+          "hyprland/workspaces#chinese"
+          "hyprland/workspaces#english"
+        ];
         modules-center = ["custom/host"];
         # 👇 add GPU info next to clock
         modules-right = ["custom/gpu" "pulseaudio" "clock" "network" "tray"];
@@ -55,6 +68,94 @@
           '';
           interval = "once";
           return-type = "json";
+        };
+
+        "niri/workspaces#japanese" = {
+          format = "{icon}";
+          format-icons = {
+            "1" = "いち";
+            "2" = "に";
+            "3" = "さん";
+            "4" = "よん";
+            default = "〇";
+          };
+        };
+
+        "niri/workspaces#korean" = {
+          format = "{icon}";
+          format-icons = {
+            "1" = "하나";
+            "2" = "둘";
+            "3" = "셋";
+            "4" = "넷";
+            default = "〇";
+          };
+        };
+
+        "niri/workspaces#chinese" = {
+          format = "{icon}";
+          format-icons = {
+            "1" = "一";
+            "2" = "二";
+            "3" = "三";
+            "4" = "四";
+            default = "〇";
+          };
+        };
+
+        "niri/workspaces#english" = {
+          format = "{icon}";
+          format-icons = {
+            "1" = "1";
+            "2" = "2";
+            "3" = "3";
+            "4" = "4";
+            default = "0";
+          };
+        };
+
+        "hyprland/workspaces#japanese" = {
+          format = "{icon}";
+          format-icons = {
+            "1" = "いち";
+            "2" = "に";
+            "3" = "さん";
+            "4" = "よん";
+            default = "〇";
+          };
+        };
+
+        "hyprland/workspaces#korean" = {
+          format = "{icon}";
+          format-icons = {
+            "1" = "하나";
+            "2" = "둘";
+            "3" = "셋";
+            "4" = "넷";
+            default = "〇";
+          };
+        };
+
+        "hyprland/workspaces#chinese" = {
+          format = "{icon}";
+          format-icons = {
+            "1" = "一";
+            "2" = "二";
+            "3" = "三";
+            "4" = "四";
+            default = "〇";
+          };
+        };
+
+        "hyprland/workspaces#english" = {
+          format = "{icon}";
+          format-icons = {
+            "1" = "1";
+            "2" = "2";
+            "3" = "3";
+            "4" = "4";
+            default = "0";
+          };
         };
 
         # 🧩 GPU usage + temp via nvidia-smi
